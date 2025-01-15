a395e4dce48392a69120799095cfa1ee134dc873
Colin Frei <colin@colinfrei.com>
2025-01-15 10:26:14 +0100

    fix missing quote

--- regex_validator.py
+++ regex_validator.py
@@ -8,7 +8,7 @@ pattern = r'\d\d\.\d\d\.\d\d\d\d \d\d\:\d\d\; ((Nachalarmierung), )?(.*?)?, in (
 # Define the examples and expected group values
 examples = [
     ('15.01.2023 16:08; BMA, in Seon, Oberdorfstrasse 33, Stiftung ABC, Hauptgebäude,', ['BMA', 'Seon', 'Oberdorfstrasse 33', 'Stiftung ABC, Hauptgebäude,']),
-    ('06.02.2021 14:21; Brand-Mittel, in Seon, Hansligasse 432, Holzstapel', ['Brand-Mittel', 'Seon', 'Hansligasse 432', Holzstapel'])
+    ('06.02.2021 14:21; Brand-Mittel, in Seon, Hansligasse 432, Holzstapel', ['Brand-Mittel', 'Seon', 'Hansligasse 432', 'Holzstapel'])
 ]
 
 def validate_examples(pattern, examples):
